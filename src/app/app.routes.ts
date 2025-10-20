5fffda1d1654f8522864196a613e72df911fc8e3
Brenda2310 <brendafernandez2310@gmail.com>
2025-10-20 18:47:29 -0300

    Login added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth/auth-guard';
-import { NotFound } from './shared/not-found/not-found';
-import { Home } from './pages/home/home';
+import { NotFound } from './not-found/not-found';
+import { Home } from './home/home';
+import { Login } from './security/login/login/login';
 
 export const routes: Routes = [
 
-  { path: '', component: Home },
-  { path: '**', component: NotFound }
+  { path: '', component: Home, title: 'Inicio'},
+  { path: 'login', component: Login, title:'Iniciar Sesión'},
+  { path: '**', component: NotFound, title:'Not Found'}
 ];
---
+++ src/app/security/login/login/login.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SecurityStore } from '../../services/security-store';
+import { Router } from '@angular/router';
+import { AuthRequest } from '../../security-models';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login implements OnInit{
+  public readonly store = inject(SecurityStore);
+  private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
+
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+   ngOnInit(): void {
+    if (this.store.auth().isAuthenticated) {
+      this.router.navigate(['/']);
+    }
+
+    const authRequest: AuthRequest = { 
+        email: this.loginForm.value.email!,
+        password: this.loginForm.value.password! 
+    };
+
+    this.store.authenticateUser(authRequest).subscribe({
+      next: () => {
+        this.router.navigate(['/']); 
+      },
+      error: () => {
+        console.log("Error handled by signal display.");
+      }
+    });
+  }
+}
