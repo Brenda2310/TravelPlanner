1d0cc5dc55cf473ab5a49e3b68e54e4959274407
Melina Di Meglio <meli.dime@gmail.com>
2025-10-22 11:42:04 -0300

    style changes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Login } from '../security/auth/login/login';
 import { AuthStatus } from '../security/auth/logout/auth-status/auth-status';
 import { RouterModule } from '@angular/router';
+import { Testimonials } from "./testimonials/testimonials/testimonials";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, Testimonials],
   standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.css']
