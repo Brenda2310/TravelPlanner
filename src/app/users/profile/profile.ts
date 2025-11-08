31124e258300732bc311f14075c40c9aba2c186f
Melina Di Meglio <meli.dime@gmail.com>
2025-11-08 12:19:14 -0300

    profile css

--- src/app/users/profile/profile.ts
+++ src/app/users/profile/profile.ts
@@ -1,8 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserStore } from '../services/user-store';
 import { SecurityStore } from '../../security/services/security-store';
 import { CommonModule } from '@angular/common';
+import { TripStore } from '../../trips/services/trip-store';
+import { Pageable } from '../../hateoas/hateoas-models';
 
 @Component({
   selector: 'app-profile',
@@ -14,11 +16,41 @@ export class Profile implements OnInit{
 private readonly router = inject(Router);
     public readonly userStore = inject(UserStore);
     public readonly securityStore = inject(SecurityStore);
+    public readonly store = inject(TripStore);
+    public pageable: Pageable ={page: 0, size: 10, sort: 'startDate,desc'};
+
+  @Input() mode: 'admin-all' | 'user-own' = 'user-own';
+
+  loadTrips(){
+    if(this.mode === "admin-all"){
+      this.store.loadAllTrips(this.pageable);
+    }
+    else{
+      const currentUserId = this.securityStore.getId();
+
+        if(currentUserId !== null){
+          const filters = {}; 
+          this.store.loadTripsByUserId(currentUserId, filters, this.pageable);
+        }
+        else {
+            console.log("Error: Usuario no autenticado.");
+        }
+      }
+    }
+  
+
+  onPageChange(newPage: number): void {
+    this.pageable.page = newPage;
+    this.loadTrips();
+  }
+
 
     public currentUser = this.userStore.profile; 
 
     ngOnInit(): void {
         this.userStore.loadProfile();
+        this.loadTrips();
+
     }
 
     onEditProfile(): void {
